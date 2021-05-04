21171ceba3bb734a33b818174515c9916fdc2194
nvlozano <nailavanessa2009_@hotmail.com>
2021-05-04 12:26:32 -0500

    agregado dotenv a la API

--- api/src/db.js
+++ api/src/db.js
@@ -1,7 +1,6 @@
 var Sequelize = require('sequelize');
-var DataTypes = Sequelize.DataTypes;
 const S = Sequelize;
-const {DB_PASSWORD, DB_HOST, DB_USER} = require('../env')
+const {DB_PASSWORD, DB_HOST, DB_USER} = process.env;
 
 var db = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/chatdb`, {
   logging: false,
